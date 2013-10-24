3b89ec5cc280079392ccc32a2239b0b598d03499
liumx <liumx11@sei.pku.edu.cn>
2013-10-25 00:27:25 +0800

    optimize the solution and add time test

--- problem1.py
+++ problem1.py
@@ -7,5 +7,18 @@ Multiples of 3 and 5
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 Find the sum of all the multiples of 3 or 5 below 1000.
 '''
-result = sum([i for i in range(1000) if i%3 == 0 or i % 5 == 0])
-print str(result)
+import timeit
+
+def sol1():
+    return sum((i for i in range(1000) if i%3 == 0 or i % 5 == 0))
+
+def sol2():
+    threes = sum((i*3 for i in range(1000/3 + 1)))
+    fives = sum((i*5 for i in range(1000/5)))
+    fifteens = sum((i*15 for i in range(1000/15 + 1)))
+    return threes + fives - fifteens
+print str(sol1())
+print str(sol2())
+
+print timeit.timeit('sol1()',"from __main__ import sol1", number = 10000)
+print timeit.timeit('sol2()',"from __main__ import sol2", number = 10000)
